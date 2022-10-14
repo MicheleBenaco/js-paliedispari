0f6c0e45b86863c71dccbc45b3cae3a092920de8
MicheleBenaco <benaco.michele@yahoo.it>
2022-10-14 15:57:01 +0200

    funzione

--- js/script.js
+++ js/script.js
@@ -2,10 +2,11 @@ const parolaUtente = document.getElementById('parolaUtente')
 const bottone = document.getElementById('btnStart');
 const soluzione = document.getElementById('soluzione')
 
-bottone.addEventListener('click',isPalindrome);{
-    if(isPalindrome === true){
+bottone.addEventListener('click',isPalindrome(parolaUtente) )
+    if(isPalindrome(string) === true){
         soluzione.innerText = ' La parola e palidrome'
     }else{
         soluzione.innerText = ' La parola non è palidroma'
     }
-}
+
+

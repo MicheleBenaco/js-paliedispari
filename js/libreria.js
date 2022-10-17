1d7ec1149932f65a34703bfde27acc93377aec86
MicheleBenaco <benaco.michele@yahoo.it>
2022-10-17 10:06:50 +0200

    es1

--- js/libreria.js
+++ js/libreria.js
@@ -1,18 +1,40 @@
-function isPalindrome(string) {
-    if (string.length <= 1) {
+
+//  export const pali =  function isPalindrome(string) {
+//     if (string.length <= 1) {
+//       return true;
+//     }
+  
+//     let [firstLetter] = string;
+//     let lastLetter = string[string.length - 1];
+  
+//     if (firstLetter === lastLetter) {
+//       let stringWithoutFirstAndLastLetters = string.substring(
+//         1,
+//         string.length - 1
+//       );
+//       return isPalindrome(stringWithoutFirstAndLastLetters);
+//     } else {
+//       return false;
+//     }
+//   }
+
+  export function isPalindrome(string) {
+    string = trasformStringUpperCase(string)
+    const parolaRevers = [];
+    const lunghezza = string.length;
+    for(let i = lunghezza; i > 0; i--){
+      parolaRevers.push(string.charAt(i-1));
+      
+    }
+    const stringap = parolaRevers.join('');
+    if(stringap == string){
       return true;
     }
-  
-    let [firstLetter] = string;
-    let lastLetter = string[string.length - 1];
-  
-    if (firstLetter === lastLetter) {
-      let stringWithoutFirstAndLastLetters = string.substring(
-        1,
-        string.length - 1
-      );
-      return isPalindrome(stringWithoutFirstAndLastLetters);
-    } else {
-      return false;
-    }
+    return false;
+    
+  }
+
+  function trasformStringUpperCase(string){
+    return string.toUpperCase();
+
   }
--- js/script.js
+++ js/script.js
@@ -1,14 +1,26 @@
+import {isPalindrome} from './libreria.js';
 const parolaUtente = document.getElementById('parolaUtente')
 const bottone = document.getElementById('btnStart');
-const soluzione = document.getElementById('soluzione');
+const showResponse = document.getElementById('soluzione');
 
-bottone.addEventListener('click', isPalindrome(){
-    if(isPalindrome(parolaUtente.length)) {
-        soluzione.innerText = 'La parola è palidroma ';    
+
+
+// bottone.addEventListener('click', pali());
+
+
+
+
+bottone.addEventListener('click', callBack)
+       
+  
+ function callBack (){
+    const result =  isPalindrome(parolaUtente.value);
+    if(result === true) {
+        showResponse.innerText = 'La parola è palidroma ';    
     }else{
-        soluzione.innerText = 'La parola non è palidroma ';
+        showResponse.innerText = 'La parola non è palidroma ';
     }
 
-})
+} 
 
-
+// bottone.addEventListener('click', callBack)
